b776cb7f0c25919b0136f9c2d575defb980c9fff
ashuvinayak1508 <ashuvinayak1508@gmail.com>
2023-10-24 23:26:03 +0530

    initial commit

---
+++ public/sricpt.js
@@ -0,0 +1,19 @@
+var typed = new Typed(".typing", {
+  strings: ["CORPORATE EVENTS", "EDUCATIONAL EVENTS", "WEDDINGS", "PARTIES"],
+  typespeed: 0.0090,
+  backspeed: 10,
+  backDelay: 5000,
+  typeDelay: 5000,
+  loop: true
+});
+
+function save() {
+  const name = document.getElementById("name").value;
+  const num = document.getElementById("num").value;
+  const mail = document.getElementById("mail").value;
+  const event = document.getElementById("event").value;
+  const message = document.getElementById("message").value;
+  
+
+
+}
